2a305206ded54ad93c18ad612611eb3e079ea222
uecken <the.full.of.moon@gmail.com>
2026-05-20 13:26:12 +0900

    wand_receiver: WINGARDIUM 受信レート(Hz)メーター追加 (送信/受信ボトルネック判別用)

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@
 import { h, render } from 'preact';
 import { useState, useEffect, useRef, useCallback } from 'preact/hooks';
 import htm from 'htm';
-import { SerialClient } from './lib/SerialClient.js?v=20260520-130944';
-import { BleClient }    from './lib/BleClient.js?v=20260520-130944';
-import { IMUViewer }    from './lib/IMUViewer.js?v=20260520-130944';
-import { RelativeIMUViewer } from './lib/RelativeIMUViewer.js?v=20260520-130944';
-import { RelativeTrajectoryGrid } from './lib/RelativeTrajectoryGrid.js?v=20260520-130944';
-import { PitchRollGrid } from './lib/PitchRollGrid.js?v=20260520-130944';
-import { TimeSeriesChart } from './lib/TimeSeriesChart.js?v=20260520-130944';
+import { SerialClient } from './lib/SerialClient.js?v=20260520-132612';
+import { BleClient }    from './lib/BleClient.js?v=20260520-132612';
+import { IMUViewer }    from './lib/IMUViewer.js?v=20260520-132612';
+import { RelativeIMUViewer } from './lib/RelativeIMUViewer.js?v=20260520-132612';
+import { RelativeTrajectoryGrid } from './lib/RelativeTrajectoryGrid.js?v=20260520-132612';
+import { PitchRollGrid } from './lib/PitchRollGrid.js?v=20260520-132612';
+import { TimeSeriesChart } from './lib/TimeSeriesChart.js?v=20260520-132612';
 
 const html = htm.bind(h);
 
