d68c35cb8d84337a8123462e0565a8f33797f803
uecken <the.full.of.moon@gmail.com>
2026-05-20 12:36:04 +0900

    wand_receiver: リロード/離脱/非表示で scan停止 (Android scanner リーク&OPPORTUNISTIC落ち防止)

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@
 import { h, render } from 'preact';
 import { useState, useEffect, useRef, useCallback } from 'preact/hooks';
 import htm from 'htm';
-import { SerialClient } from './lib/SerialClient.js?v=20260520-121858';
-import { BleClient }    from './lib/BleClient.js?v=20260520-121858';
-import { IMUViewer }    from './lib/IMUViewer.js?v=20260520-121858';
-import { RelativeIMUViewer } from './lib/RelativeIMUViewer.js?v=20260520-121858';
-import { RelativeTrajectoryGrid } from './lib/RelativeTrajectoryGrid.js?v=20260520-121858';
-import { PitchRollGrid } from './lib/PitchRollGrid.js?v=20260520-121858';
-import { TimeSeriesChart } from './lib/TimeSeriesChart.js?v=20260520-121858';
+import { SerialClient } from './lib/SerialClient.js?v=20260520-123604';
+import { BleClient }    from './lib/BleClient.js?v=20260520-123604';
+import { IMUViewer }    from './lib/IMUViewer.js?v=20260520-123604';
+import { RelativeIMUViewer } from './lib/RelativeIMUViewer.js?v=20260520-123604';
+import { RelativeTrajectoryGrid } from './lib/RelativeTrajectoryGrid.js?v=20260520-123604';
+import { PitchRollGrid } from './lib/PitchRollGrid.js?v=20260520-123604';
+import { TimeSeriesChart } from './lib/TimeSeriesChart.js?v=20260520-123604';
 
 const html = htm.bind(h);
 
