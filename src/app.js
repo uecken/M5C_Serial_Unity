e910623cecfa37767bbe848bb8a792a27515305c
uecken <the.full.of.moon@gmail.com>
2026-05-20 12:12:13 +0900

    wand_receiver: スマホモーションのライブ表示+感度スライダー+無イベント警告

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@
 import { h, render } from 'preact';
 import { useState, useEffect, useRef, useCallback } from 'preact/hooks';
 import htm from 'htm';
-import { SerialClient } from './lib/SerialClient.js?v=20260520-120942';
-import { BleClient }    from './lib/BleClient.js?v=20260520-120942';
-import { IMUViewer }    from './lib/IMUViewer.js?v=20260520-120942';
-import { RelativeIMUViewer } from './lib/RelativeIMUViewer.js?v=20260520-120942';
-import { RelativeTrajectoryGrid } from './lib/RelativeTrajectoryGrid.js?v=20260520-120942';
-import { PitchRollGrid } from './lib/PitchRollGrid.js?v=20260520-120942';
-import { TimeSeriesChart } from './lib/TimeSeriesChart.js?v=20260520-120942';
+import { SerialClient } from './lib/SerialClient.js?v=20260520-121212';
+import { BleClient }    from './lib/BleClient.js?v=20260520-121212';
+import { IMUViewer }    from './lib/IMUViewer.js?v=20260520-121212';
+import { RelativeIMUViewer } from './lib/RelativeIMUViewer.js?v=20260520-121212';
+import { RelativeTrajectoryGrid } from './lib/RelativeTrajectoryGrid.js?v=20260520-121212';
+import { PitchRollGrid } from './lib/PitchRollGrid.js?v=20260520-121212';
+import { TimeSeriesChart } from './lib/TimeSeriesChart.js?v=20260520-121212';
 
 const html = htm.bind(h);
 
