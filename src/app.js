0e3de8d933a3ad0a265c08098ece289f2993be14
uecken <the.full.of.moon@gmail.com>
2026-05-20 13:34:10 +0900

    wand_receiver: Expecto Patronum(0x23) 魔法画面追加(守護霊🦌+銀青光6秒) / Incendio表記=横振りに更新

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@
 import { h, render } from 'preact';
 import { useState, useEffect, useRef, useCallback } from 'preact/hooks';
 import htm from 'htm';
-import { SerialClient } from './lib/SerialClient.js?v=20260520-133043';
-import { BleClient }    from './lib/BleClient.js?v=20260520-133043';
-import { IMUViewer }    from './lib/IMUViewer.js?v=20260520-133043';
-import { RelativeIMUViewer } from './lib/RelativeIMUViewer.js?v=20260520-133043';
-import { RelativeTrajectoryGrid } from './lib/RelativeTrajectoryGrid.js?v=20260520-133043';
-import { PitchRollGrid } from './lib/PitchRollGrid.js?v=20260520-133043';
-import { TimeSeriesChart } from './lib/TimeSeriesChart.js?v=20260520-133043';
+import { SerialClient } from './lib/SerialClient.js?v=20260520-133409';
+import { BleClient }    from './lib/BleClient.js?v=20260520-133409';
+import { IMUViewer }    from './lib/IMUViewer.js?v=20260520-133409';
+import { RelativeIMUViewer } from './lib/RelativeIMUViewer.js?v=20260520-133409';
+import { RelativeTrajectoryGrid } from './lib/RelativeTrajectoryGrid.js?v=20260520-133409';
+import { PitchRollGrid } from './lib/PitchRollGrid.js?v=20260520-133409';
+import { TimeSeriesChart } from './lib/TimeSeriesChart.js?v=20260520-133409';
 
 const html = htm.bind(h);
 
