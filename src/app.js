c6c22b497eed813015882717a841de3ce0adb352
uecken <the.full.of.moon@gmail.com>
2026-05-20 12:03:10 +0900

    wand_receiver: ボタン廃止→魔法画面ダブルタップで拡大/通常トグル

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@
 import { h, render } from 'preact';
 import { useState, useEffect, useRef, useCallback } from 'preact/hooks';
 import htm from 'htm';
-import { SerialClient } from './lib/SerialClient.js?v=20260520-115435';
-import { BleClient }    from './lib/BleClient.js?v=20260520-115435';
-import { IMUViewer }    from './lib/IMUViewer.js?v=20260520-115435';
-import { RelativeIMUViewer } from './lib/RelativeIMUViewer.js?v=20260520-115435';
-import { RelativeTrajectoryGrid } from './lib/RelativeTrajectoryGrid.js?v=20260520-115435';
-import { PitchRollGrid } from './lib/PitchRollGrid.js?v=20260520-115435';
-import { TimeSeriesChart } from './lib/TimeSeriesChart.js?v=20260520-115435';
+import { SerialClient } from './lib/SerialClient.js?v=20260520-120309';
+import { BleClient }    from './lib/BleClient.js?v=20260520-120309';
+import { IMUViewer }    from './lib/IMUViewer.js?v=20260520-120309';
+import { RelativeIMUViewer } from './lib/RelativeIMUViewer.js?v=20260520-120309';
+import { RelativeTrajectoryGrid } from './lib/RelativeTrajectoryGrid.js?v=20260520-120309';
+import { PitchRollGrid } from './lib/PitchRollGrid.js?v=20260520-120309';
+import { TimeSeriesChart } from './lib/TimeSeriesChart.js?v=20260520-120309';
 
 const html = htm.bind(h);
 
